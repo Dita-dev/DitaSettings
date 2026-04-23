b09cbe49f2deb1292372c5bf69872ae2576fd33e
Dita <DitaTVGaming@gmail.com>
2026-04-23 16:38:50 +0200

    Moved the backup code to a seperate file so i can keep my test.py somewhat clean

---
+++ backupHyprConf.py
@@ -0,0 +1,27 @@
+def backupHyprConf():
+    i = 0
+    FileCheck = False
+    while FileCheck == False:
+        try:
+            if i == 0:
+                backupfile = open("/home/dita/.config/hypr/backup/hyprland.backup.conf","r")
+            else: 
+                backupfile = open(f"/home/dita/.config/hypr/backup/hyprland.backup.conf({i})","r")
+            backupfile.close()
+        except:
+            FileCheck = True
+            print("Passed")
+            if i == 0:
+                file = open("/home/dita/.config/hypr/backup/hyprland.backup.conf","w")
+            else: 
+                file = open(f"/home/dita/.config/hypr/backup/hyprland.backup.conf({i})","w")
+
+            hyprconf = open("/home/dita/.config/hypr/hyprland.conf","r")
+            file.write(hyprconf.read())
+
+            hyprconf.close()
+            file.close()
+
+        else: 
+            print("Failed")
+            i += 1
